7cd2f95432dc65826c5b1ddc2711f80a6ab62874
Jinyoung <glitt5384@naver.com>
2025-10-03 01:44:39 +0900

    feat: test pg 서버와 통신하는 클라이언트

---
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/TestPgClient.kt
@@ -0,0 +1,40 @@
+package im.bigs.pg.external.pg
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import im.bigs.pg.application.pg.port.out.PgApproveRequest
+import im.bigs.pg.application.pg.port.out.PgApproveResult
+import im.bigs.pg.application.pg.port.out.PgClientOutPort
+import im.bigs.pg.application.pg.port.out.TestPgApproveResult
+import im.bigs.pg.external.pg.config.TestPgProperties
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component
+class TestPgClient(
+    private val webClientBuilder: WebClient.Builder,
+    private val props: TestPgProperties
+):  PgClientOutPort{
+    private val baseUrl = "https://api-test-pg.bigs.im"
+    private val objectMapper = jacksonObjectMapper()
+    override fun supports(partnerCode: String): Boolean = partnerCode.contains("TESTPAY")
+
+    override fun approve(request: PgApproveRequest): PgApproveResult {
+        // 예: 암호화 호출
+        val apiKey = props.apiKey
+        val enc = props.enc
+
+        val webClient = webClientBuilder.baseUrl(baseUrl).build()
+        val response = webClient.post()
+            .uri("/api/v1/pay/credit-card")
+            .header("API-KEY", apiKey)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(mapOf("enc" to enc))
+            .retrieve()
+            .bodyToMono(TestPgApproveResult::class.java)
+            .block() ?: throw IllegalStateException("PG Server Response is not expected.")
+
+        return PgApproveResult.from(response)
+
+    }
+}
