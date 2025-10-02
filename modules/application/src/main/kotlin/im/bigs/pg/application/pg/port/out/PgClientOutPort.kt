0ae73ee4ddd9693732a37cdec2cd65451e22a680
Jinyoung <glitt5384@naver.com>
2025-10-03 01:40:23 +0900

    feat: support 함수의 매개변수 변경; partnerId -> partnerCode

--- modules/application/src/main/kotlin/im/bigs/pg/application/pg/port/out/PgClientOutPort.kt
+++ modules/application/src/main/kotlin/im/bigs/pg/application/pg/port/out/PgClientOutPort.kt
@@ -2,6 +2,6 @@ package im.bigs.pg.application.pg.port.out
 
 /** 외부 결제사(PG) 승인 연동 포트. */
 interface PgClientOutPort {
-    fun supports(partnerId: Long): Boolean
+    fun supports(partnerCode: String): Boolean
     fun approve(request: PgApproveRequest): PgApproveResult
 }
