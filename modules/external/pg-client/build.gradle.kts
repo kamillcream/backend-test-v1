efd794dd74e087d71470949cd264a6e8595dacf0
Jinyoung <glitt5384@naver.com>
2025-10-03 01:39:43 +0900

    feat: testpg 서버 연동에 사용할 webclient를 위한 의존성 추가

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(projects.modules.domain)
     implementation(libs.spring.boot.starter.web)
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 repositories {
     mavenCentral()
