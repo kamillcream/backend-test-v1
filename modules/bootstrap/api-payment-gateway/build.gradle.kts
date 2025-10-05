19177c4840c1ebface01466b897c72c241a1e012
Jinyoung <glitt5384@naver.com>
2025-10-05 19:04:55 +0900

    feat: 스웨거 문서 의존성 추가

--- modules/bootstrap/api-payment-gateway/build.gradle.kts
+++ modules/bootstrap/api-payment-gateway/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation(libs.spring.mockk)
     testImplementation(libs.database.h2)
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 }
 repositories {
     mavenCentral()
