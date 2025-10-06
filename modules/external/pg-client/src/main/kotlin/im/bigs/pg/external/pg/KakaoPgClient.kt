82ea7ac47d8bee561144260c248f46850ccd65c9
Jinyoung <glitt5384@naver.com>
2025-10-07 02:11:30 +0900

    feat: 카카오페이 결제 클라이언트

---
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/KakaoPgClient.kt
@@ -0,0 +1,43 @@
+package im.bigs.pg.external.pg
+
+import im.bigs.pg.application.pg.port.out.PgApproveRequest
+import im.bigs.pg.application.pg.port.out.PgApproveResult
+import im.bigs.pg.application.pg.port.out.PgClientOutPort
+import im.bigs.pg.domain.payment.PaymentStatus
+import im.bigs.pg.external.pg.dto.KakaoPayRequestDto
+import im.bigs.pg.external.pg.service.KakaoPayService
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+
+@Component
+class KakaoPgClient(
+    private val kakaoPayService: KakaoPayService
+) : PgClientOutPort {
+
+    override fun supports(partnerCode: String): Boolean = partnerCode.contains("KAKAOPAY", ignoreCase = true)
+
+    override fun approve(request: PgApproveRequest): PgApproveResult {
+        val kakaoRequest = KakaoPayRequestDto(
+            cid = "TC0ONETIME",
+            partnerOrderId = request.partnerId.toString(),
+            partnerUserId = "TEST_USER1",
+            itemName = "TEST_ITEM1",
+            quantity = 3,
+            totalAmount = request.amount.toInt(),
+            taxFreeAmount = 0,
+            approvalUrl = "http://localhost:8080" + "/success",
+            failUrl = "http://blaybus-glowup.com" + "/fail",
+            cancelUrl = "http://blaybus-glowup.com" + "/cancel",
+
+        )
+
+        val kakaoResponse = kakaoPayService.kakaoPayReady(kakaoRequest)
+
+        return PgApproveResult(
+            approvalCode = kakaoResponse.tid,
+            approvedAt = LocalDateTime.now(),
+            status = PaymentStatus.APPROVED
+        )
+    }
+}
