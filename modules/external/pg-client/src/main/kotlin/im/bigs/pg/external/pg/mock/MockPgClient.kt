2243acaf2fba29fb9841fb16ac3247463b222b42
Jinyoung <glitt5384@naver.com>
2025-10-07 13:32:25 +0900

    chore: 디렉토리 변경

--- modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/mock/MockPgClient.kt
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/mock/MockPgClient.kt
@@ -1,13 +1,13 @@
-package im.bigs.pg.external.pg
+package im.bigs.pg.external.pg.mock
 
 import im.bigs.pg.application.pg.port.out.PgApproveRequest
 import im.bigs.pg.application.pg.port.out.PgApproveResult
 import im.bigs.pg.application.pg.port.out.PgClientOutPort
 import im.bigs.pg.domain.payment.PaymentStatus
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
-import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import kotlin.random.Random
 
@@ -21,7 +21,7 @@ class MockPgClient : PgClientOutPort {
 
     override fun approve(request: PgApproveRequest): PgApproveResult {
         val dateOfMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("MMdd"))
-        val randomDigits = Random.nextInt(9999).toString().padStart(4, '0')
+        val randomDigits = Random.Default.nextInt(9999).toString().padStart(4, '0')
         return PgApproveResult(
             approvalCode = "$dateOfMonth$randomDigits",
             approvedAt = LocalDateTime.now(ZoneOffset.UTC),
